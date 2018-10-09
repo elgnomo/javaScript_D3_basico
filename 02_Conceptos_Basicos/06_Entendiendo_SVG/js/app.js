e777d34afc62dfe6448b9336d0d8681b9ef1ba0d
elGnomo <efrainantonio@ideashappy.com>
2018-10-09 02:02:20 -0500

    Entendien SVG

---
+++ 02_Conceptos_Basicos/06_Entendiendo_SVG/js/app.js
@@ -0,0 +1,14 @@
+"use strict"
+
+var datos = [1, 2, 3, 5, 8, 11, 13, 21, 34, 55, 89, 144]
+
+function graficar() {
+    d3.select('.miGrafica')
+        .selectAll('div')
+        .data(datos)
+        .enter().append('div')
+        .attr('class', 'barra') //atributo
+        .style('height', function (d) {
+            return d + 'px';
+        })
+}
